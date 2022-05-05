af731947791d261cc952d01c97169f0d67c739c8
matte887 <m.erroi92@gmail.com>
2022-05-05 14:44:05 +0200

    Aggiunto titolo e immagine con vue

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Hello World!',
+            catImage: 'https://placekitten.com/640/640'
+        }
+    }
+)
